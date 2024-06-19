76189e424cd42c0c6ff4849929a1ee9030fa41a7
Mridul Islam <md.mridul.23@gmail.com>
2024-06-19 17:41:20 +0600

    wizard page added

---
+++ lib/currency.ts
@@ -0,0 +1,10 @@
+//according to MDN web docs INTI.NumberFormat
+
+export const Currencies = [
+  { value: 'USD', label: '$ Dollar', locale: 'en-US' },
+  { value: 'EUR', label: '€ Euro', locale: 'de-DE' },
+  { value: 'JPY', label: '¥ Yen', locale: 'ja-JP' },
+  { value: 'GBP', label: '£ Pound', locale: 'en-GB' }
+];
+
+export type Currency = (typeof Currencies)[0];
